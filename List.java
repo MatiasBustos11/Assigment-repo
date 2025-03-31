9296f8f1c7826ed96fec0931b7fbe2ac00934946
Matt <kb1303@mdx.live.ac.uk>
2025-03-31 23:57:42 +0100

    Imported java.util.ArrayList

---
+++ List.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class MergeSortSimple {
+
+    public static void main(String[] args) {
+        // Step 1: Create an empty list of words
+        ArrayList<String> words = new ArrayList<>();
